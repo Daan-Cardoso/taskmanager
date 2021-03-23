6e588b143351a403913dcba91eaf2d0791c05c95
Danilo Cardoso <67012319+Daan-Cardoso@users.noreply.github.com>
2021-03-22 21:49:20 -0300

    Button Add Task and modify style.scss from ButtonMenu

---
+++ src/components/ButtonAddTask/ButtonAddTask.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="buttons addTask">
+      <i class="addTask__icon"></i>
+    </button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonAddTask',
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './style.scss';
+</style>

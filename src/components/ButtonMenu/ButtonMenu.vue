dacd02640658ac4a27f0fbffba95d73a7af33eff
Danilo Cardoso <67012319+Daan-Cardoso@users.noreply.github.com>
2021-03-22 21:33:11 -0300

    Menu Button

---
+++ src/components/ButtonMenu/ButtonMenu.vue
@@ -0,0 +1,31 @@
+<template>
+  <button @click="toggleMenuState()" class="buttons menu">
+  <i
+    v-bind:class="{'menu__icon--active' : this.menuOpen}"
+    class="menu__icon"
+  ></i>
+</button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonMenu',
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      menuOpen: false
+    }
+  },
+  methods: {
+    toggleMenuState () {
+      this.menuOpen = !this.menuOpen
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './style.scss';
+</style>
